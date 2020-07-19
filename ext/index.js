e8eae2843e282aa71ce024f61e79d64647b319cb
Adam Mathieson <mathieson1222@gmail.com>
2020-07-19 16:31:27 -0700

    bg animation translateX to for walking effect

--- ext/index.js
+++ ext/index.js
@@ -1,9 +1,12 @@
 
 const tlWalk = gsap.timeline({repeat: -1, yoyo: true})
 
-tlWalk.set("#guy-back", {display: "none"})
 tlWalk.set("#guy-back", {transformOrigin: "50%, 0%"})
 
-
 tlWalk.fromTo("#leg-right", {rotate: 10}, {rotate: -10, ease: "none"})
-tlWalk.fromTo("#leg-left", {rotate: 10}, {rotate: -10, ease: "none"})
+tlWalk.fromTo("#leg-left", {rotate: 10}, {rotate: -10, ease: "none", alternate: "reverse"})
+
+// tlWalk.pause()
+
+const tlBg = gsap.timeline()
+tlBg.to("#scene-container", {translateX: "-1040", duration: 7, ease: 'none'})
