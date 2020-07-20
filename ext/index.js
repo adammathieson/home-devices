5c695b930243f7c9df3c85dc98b3e909c1abd234
Adam Mathieson <mathieson1222@gmail.com>
2020-07-19 19:16:18 -0700

    animaion of walk up to front door rough draft complete

--- ext/index.js
+++ ext/index.js
@@ -1,12 +1,39 @@
 
-const tlWalk = gsap.timeline({repeat: -1, yoyo: true})
+const rLeg = TweenMax.fromTo("#leg-right", .7, {rotate: 10}, {rotate: -10, ease:Linear.easeNone, repeat:-1, yoyo: true});
+rLeg.progress(0.5).pause();
+rLeg.play();
 
-tlWalk.set("#guy-back", {transformOrigin: "50%, 0%"})
+const lLeg = TweenMax.fromTo("#leg-left", .7, {rotate: -10}, {rotate: 10, ease:Linear.easeNone, repeat:-1, yoyo: true});
+lLeg.progress(0.5).pause();
+lLeg.play();
 
-tlWalk.fromTo("#leg-right", {rotate: 10}, {rotate: -10, ease: "none"})
-tlWalk.fromTo("#leg-left", {rotate: 10}, {rotate: -10, ease: "none", alternate: "reverse"})
+const rLegBack = TweenMax.fromTo("#leg-right-back", .7, {rotate: 7}, {rotate: -7, ease:Linear.easeNone, repeat:-1, yoyo: true});
+rLegBack.progress(0.5).pause();
+rLegBack.play();
+
+const lLegBack = TweenMax.fromTo("#leg-left-back", .7, {rotate: -7}, {rotate: 7, ease:Linear.easeNone, repeat:-1, yoyo: true});
+lLegBack.progress(0.5).pause();
+lLegBack.play();
+
+const stopWalking = () => {
+    lLegBack.pause()
+    rLegBack.pause()
+}
+
+// const tlWalk = gsap.timeline({repeat: -1, duration: .5, yoyo: true})
+
+// tlWalk.set("#guy-back", {transformOrigin: "50%, 0%"})
+
+// tlWalk.fromTo("#leg-right", {rotate: 10}, {rotate: -10, ease: Linear.easeNone})
+// tlWalk.fromTo("#leg-left", {rotate: -10}, {rotate: 10, ease: "none"}, '-=1')
 
 // tlWalk.pause()
 
-const tlBg = gsap.timeline()
-tlBg.to("#scene-container", {translateX: "-1040", duration: 7, ease: 'none'})
+const tl = gsap.timeline()
+    tl.to("#scene-container", {translateX: "-995", duration: 7, ease: 'none'})
+    tl.to("#guy-side", {display: "none"},"-=0.5")
+    tl.to("#guy-back", {display: "block"})
+    tl.to("#guy-back", 3, {scale: 0.43, y:22, x:5, onComplete: stopWalking})
+    tl.to("#scene-1", {attr:{viewBox: "150 250 207 448"}, duration: 3}, "-=3")
+
+    // attr:{viewBox: "0 250 414 400"}
